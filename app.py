46c667a15548f9752781445d2c2f78577f3dbb6f
Ashish Singh <138526056+rj-ash@users.noreply.github.com>
2025-04-25 22:31:07 +0530

    added cors for local host as well as lovable api

--- app.py
+++ app.py
@@ -12,10 +12,13 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://flow-forge-campaigns.lovable.app/"],  # Allow your frontend origin
+    allow_origins=[
+        "https://flow-forge-campaigns.lovable.app",
+        "http://localhost:8080",  # for local dev
+    ],
     allow_credentials=True,
-    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
-    allow_headers=["*"],  # Allow all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.post("/send-emails", response_model=dict)
